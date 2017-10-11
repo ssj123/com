d286c48f40d2c543a9d4b1cc26372e5050079bb2
ssj123 <1018609239@qq.com>
2017-10-11 14:44:19 +0800

    ssj

--- js/main.js
+++ js/main.js
@@ -14,30 +14,33 @@ var second; //歌曲总时间
 var musicName = document.getElementById('musicName');
 var next = document.getElementById('next');
 var pre = document.getElementById('pre');
-
 // 点击播放暂停
 player.onclick = function () {
 	if( audio.paused){
+		timer = setInterval(canpaly,1000);
 		audio.play();
 		player.src = 'images/stpp.png';
 		
 	}else{
+		clearInterval(timer);
 		audio.pause();
 		player.src = 'images/play.png';
 	}
 }
 
-// 上一首下一首
+// 上一首下一首s
 	next.onclick = function next() {
 		n++;
 	
 		musicName.innerHTML = arr[n];
 		audio.src = 'lib/'+arr[n]+'.mp3'
 
-			if(n == arr.length-1){
+		if(n == arr.length-1){
 			n = -1;
 		}
-		player.src = 'images/play.png';
+		player.src = 'images/stpp.png';
+		audio.play();
+		timer = setInterval(canpaly,1000);
 	}
 
 	pre.onclick = function () {
@@ -47,7 +50,9 @@ player.onclick = function () {
 		}
 		musicName.innerHTML = arr[n];
 		audio.src = 'lib/'+arr[n]+'.mp3'
-		player.src = 'images/play.png';
+		player.src = 'images/stpp.png';
+		audio.play();
+		timer = setInterval(canpaly,1000);
 	}
 
 // 循环播放
@@ -58,7 +63,6 @@ var num = 1;
 	audio.onended = function () {
 		musicName.innerHTML = arr[n];
 		audio.src = 'lib/'+arr[n]+'.mp3'
-		console.log(n)
 		if(n == arr.length-1){
 			n = -1;
 			}
@@ -74,7 +78,6 @@ onex.onclick = function () {
 			n++;
 			musicName.innerHTML = arr[n];
 			audio.src = 'lib/'+arr[n]+'.mp3'
-			console.log(n)
 			if(n == arr.length-1){
 				n = -1;
 				}
@@ -96,39 +99,108 @@ onex.onclick = function () {
 }
 
 // 音量调节
-var bar = document.getElementById('bar')
+var bar = document.getElementById('bar');
 var current = document.getElementById('current');
-bar.onmousedown = function () {
-	alert()
-	console.log(bar.style.bottom);
-	// audio.volume = current.height;
+var toralvo = document.getElementById('event');
+var adjust = document.getElementById('adjust');
+var song = document.getElementById('song');
+var re = 1;
+
+bar.onmousedown = function (ev) {
+	var ev = ev || window.event;
+	disX = ev.clientY;
+	document.onmousemove = function (ev) {
+		var ev = ev || window.event;
+		newbom = disX - ev.clientY + 25;
+		if (newbom < 0) {
+				newbom = 0;
+			}else if( newbom > toralvo.offsetHeight){
+				newbom = toralvo.offsetHeight;
+			}
+		bar.style.bottom = newbom + 'px';
+		current.style.height = newbom + 'px';
+		audio.volume = newbom*2/100;
+		if( audio.volume == 0){
+			song.src = 'images/songclose.png';
+		}else{
+			song.src = 'images/song_y.png';
+		}
+		}
+		document.onmouseup = function () {
+			document.onmousemove = null;
+		}
 }
+song.onclick = function () {
+	adjust.style.display = 'block';
+	if(re == 0){
+		audio.volume = 0;
+		newbom = 0;
+
+		song.src = 'images/songclose.png';
+		re = 1
+	}else if( re == 1){
+		audio.volume = 0.5;
+		song.src = 'images/song_y.png';
+		newbom = toralvo.offsetHeight/2;
+		re = 0;
+	}
+	bar.style.bottom = newbom + 'px';
+	current.style.height = newbom + 'px';
+
+}
+bar.onmousemove = function () {
+	adjust.style.display = 'block';
+}
+bar.onmouseout = function () {
+	adjust.style.display = 'none';
+}
+song.onmousemove = function () {
+	adjust.style.display = 'block';
+}
+song.onmouseout = function () {
+	adjust.style.display = 'none';
+}
+toralvo.onmousemove = function () {
+	adjust.style.display = 'block';
+}
+toralvo.onmouseout = function () {
+	adjust.style.display = 'none';
+}
+
 
  // 进度条
  var progress = document.getElementById('progress');
  var nowPro = document.getElementById('nowPro');
-
+ var time = document.getElementById('time');
 // 歌曲总时间
 function canpaly() {
-	second = parseInt(audio.seekable.end(0));
-	var m = parseInt(second/60),s = second - m*60;
+	// second = parseInt(audio.seekable.end(0));
+	second = parseInt(audio.duration);
+	var m = parseInt(second/60),s = toZero(second - m*60);
 	var sec = parseInt(audio.currentTime);
-	var m1 = parseInt(sec/60),s1 = sec - m1*60;
+	var m1 = parseInt(sec/60),s1 = toZero(sec - m1*60);
 	time.innerHTML = m1+":"+s1+"/"+m+":"+s;
 	nowPro.style.width = sec/second*100+'%';
 }
 // 歌曲时间
-var time = document.getElementById('time');
+
 var b = 0;
- setInterval(canpaly,1000)
-
- var timer = setInterval(function () {
-
- 	// nowPro.width = 30rem/audio.duration;
- },1000);
+ 
+var timer = setInterval(canpaly,1000);
 
 function updateTime(){
 	var ct = parseInt(audio.currentTime);
 
 }
+
+// 补0
+function toZero(zero) {
+	if(zero <= 9){
+		return '0'+zero;
+	}else{
+		return ''+zero;
+	}
+}
+
+
 
